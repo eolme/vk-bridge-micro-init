6fd165ff772ffdd37a3367dc6acf4926f699228c
eolme <im@petrov.engineer>
2021-03-25 14:47:49 +0400

    Add required params

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-n="VKWebAppInit",s="postMessage",i="AndroidBridge",e=window,p=parent,p!=e?p[s]({handler:n,type:"vk-connect"},"*"):e[i]?e[i][n]("{}"):e.webkit.messageHandlers[n][s]({})
+n="VKWebAppInit",s="postMessage",i="AndroidBridge",e=window,p=parent,p!=e?p[s]({handler:n,type:"vk-connect",params:{}},"*"):e[i]?e[i][n]("{}"):e.webkit.messageHandlers[n][s]({})
